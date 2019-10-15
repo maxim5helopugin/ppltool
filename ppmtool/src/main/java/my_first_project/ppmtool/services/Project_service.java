f4212cfeb45cd299134b1b33f9f86ab7d8c94901
Maxim Shelopugin <maxim.shelopugin@MSHELOPUGIN-MBP.cfl.rr.com>
2019-10-14 20:36:41 -0400

    exception handling added

--- ppmtool/src/main/java/my_first_project/ppmtool/services/Project_service.java
+++ ppmtool/src/main/java/my_first_project/ppmtool/services/Project_service.java
@@ -1,6 +1,7 @@
 package my_first_project.ppmtool.services;
 
 import my_first_project.ppmtool.domain.Project;
+import my_first_project.ppmtool.exceptions.ProjectIdException;
 import my_first_project.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ public class Project_service {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch (Exception ex){
+             throw new ProjectIdException("Project Id " + project.getProjectIdentifier().toUpperCase() + " already exists");
+        }
     }
 }
