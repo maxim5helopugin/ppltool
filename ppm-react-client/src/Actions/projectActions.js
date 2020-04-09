4946a0ec416d0115cb1d0aabc2f17d17b2de61dc
Maxim Shelopugin <maxim.shelopugin@MSHELOPUGIN-MBP.cfl.rr.com>
2020-04-09 13:06:38 -0400

    finished for now

--- ppm-react-client/src/Actions/projectActions.js
+++ ppm-react-client/src/Actions/projectActions.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./Types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./Types";
 import errorReducer from "../reducers/errorReducer";
 
 export const createProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.post("http://localhost:8080/api/project", project);
+    const res = await axios.post("/api/project", project);
     history.push("/dashboard");
     dispatch({
       type: GET_ERRORS,
@@ -19,7 +19,7 @@ export const createProject = (project, history) => async dispatch => {
 };
 
 export const getProjects = () => async dispatch => {
-  const res = await axios.get("http://localhost:8080/api/project/all");
+  const res = await axios.get("/api/project/all");
   dispatch({
     type: GET_PROJECTS,
     payload: res.data
@@ -28,12 +28,22 @@ export const getProjects = () => async dispatch => {
 
 export const getProject = (id, history) => async dispatch => {
   try {
-    const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+    const res = await axios.get(`/api/project/${id}`);
     dispatch({
       type: GET_PROJECT,
       payload: res.data
     });
   } catch (error) {
     history.push("/dashboard");
+  }
+};
+
+export const deleteProject = (id, history) => async dispatch => {
+  if (window.confirm("Are you sure?")) {
+    const res = await axios.delete(`/api/project/${id}`);
+    dispatch({
+      type: DELETE_PROJECT,
+      payload: id
+    });
   }
 };
